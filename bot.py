2ff054b0697d4c48b0cf53a70586c5eae6628bf5
lauraalmrui <83592959+lauraalmrui@users.noreply.github.com>
2024-01-31 21:53:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,27 @@
 from github import Github
+g = Github("lauraalmrui", "Monlau22")
 
-def run():
-    # Token de acceso personal de GitHub
-    # Puedes generar uno en: https://github.com/settings/tokens
-    ACCESS_TOKEN = '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'
+repo = g.get_user().get_repo(GITHUB_REPO)
+all_files = []
+contents = repo.get_contents("")
+while contents:
+    file_content = contents.pop(0)
+    if file_content.type == "dir":
+        contents.extend(repo.get_contents(file_content.path))
+    else:
+        file = file_content
+        all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
 
-    # Nombre del repositorio
-    REPO_NAME = 'almacenlar'
+with open('/tmp/file.txt', 'r') as file:
+    content = file.read()
 
-    # Ruta local del archivo que quieres subir
-    LOCAL_FILE_PATH = '/etc/passwd'
-
-    def upload_to_github():
-        # Autenticación con tu token
-        g = Github(ACCESS_TOKEN)
-
-        # Obtener el repositorio
-        repo = g.get_user().get_repo(REPO_NAME)
-
-        # Leer el contenido del archivo en modo binario
-        with open(LOCAL_FILE_PATH, 'rb') as file:
-            file_content = file.read()
-
-        # Crear un nuevo archivo en el repositorio de GitHub
-        repo.create_file(
-            path='hola.txt',
-            message='Subir hola.txt',
-            content=file_content
-        )
-
-    if __name__ == "__main__":
-        upload_to_github()
+# Upload to github
+git_prefix = 'almacenlar/'
+git_file = git_prefix + '/etc/passwd'
+if git_file in all_files:
+    contents = repo.get_contents(git_file)
+    repo.update_file(contents.path, "committing files", content, contents.sha, branch="master")
+    print(git_file + ' UPDATED')
+else:
+    repo.create_file(git_file, "committing files", content, branch="master")
+    print(git_file + ' CREATED')
