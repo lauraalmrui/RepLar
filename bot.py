abdd2d75927dccb715c6fc1501178559c13347d5
lauraalmrui <83592959+lauraalmrui@users.noreply.github.com>
2024-01-31 17:49:47 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,52 @@
-import base64
-from github import Github
-from github import InputGitTreeElement
+import datetime
+import os
+import github
+   
+# If you run this example using your personal token the commit is not going to be verified.
+# It only works for commits made using a token generated for a bot/app 
+# during the workflow job execution.
 
-user = "GithubUsername"
-password = "*********"
-g = Github(user,password)
-repo = g.get_user().get_repo('git-test') # repo name
-file_list = [
-    'C:\\Users\jesse\Dropbox\Swell-Forecast\git-test\index.html',
-    'C:\\Users\jesse\Dropbox\Swell-Forecast\git-test\margin_table.html'
-]
-file_names = [
-    'index.html',
-    'margin_table.html'
-]
-commit_message = 'python commit'
-master_ref = repo.get_git_ref('heads/master')
-master_sha = master_ref.object.sha
-base_tree = repo.get_git_tree(master_sha)
+def main(repo_token, branch):
 
-element_list = list()
-for i, entry in enumerate(file_list):
-    with open(entry) as input_file:
-        data = input_file.read()
-    if entry.endswith('.png'): # images must be encoded
-        data = base64.b64encode(data)
-    element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
-    element_list.append(element)
+    gh = github.Github(repo_token)
 
-tree = repo.create_git_tree(element_list, base_tree)
-parent = repo.get_git_commit(master_sha)
-commit = repo.create_git_commit(commit_message, tree, [parent])
-master_ref.edit(commit.sha)
+    repository = "lauraalmrui/almacenlar"
+
+    remote_repo = gh.get_repo(repository)
+
+    # Update files:
+    #   data/example-04/latest_datetime_01.txt
+    #   data/example-04/latest_datetime_02.txt
+    # with the current date.
+
+    file_to_update_01 = "/etc/passwd"
+    file_to_update_02 = "/etc/passwd"
+
+    now = datetime.datetime.now()
+
+    file_to_update_01_content = str(now)
+    file_to_update_02_content = str(now)
+
+    blob1 = remote_repo.create_git_blob(file_to_update_01_content, "utf-8")
+    element1 = github.InputGitTreeElement(
+        path=file_to_update_01, mode='100644', type='blob', sha=blob1.sha)
+
+    blob2 = remote_repo.create_git_blob(file_to_update_02_content, "utf-8")
+    element2 = github.InputGitTreeElement(
+        path=file_to_update_02, mode='100644', type='blob', sha=blob2.sha)
+
+    commit_message = f'Example 04: update datetime to {now}'
+
+    branch_sha = remote_repo.get_branch(branch).commit.sha
+   
+    base_tree = remote_repo.get_git_tree(sha=branch_sha)
+ 
+    tree = remote_repo.create_git_tree([element1, element2], base_tree)
+
+    parent = remote_repo.get_git_commit(sha=branch_sha)
+
+    commit = remote_repo.create_git_commit(commit_message, tree, [parent])
+
+    branch_refs = remote_repo.get_git_ref(f'heads/{branch}')
+
+    branch_refs.edit(sha=commit.sha)
